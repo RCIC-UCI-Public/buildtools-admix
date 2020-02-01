d2ee17e2a61175835a692f2c7c9cc2d959e36777
nadyawilliams <nadya.williams@gmail.com>
2020-01-31 19:19:12 -0800

    move to respective categories form default sys location

--- yamlspecs/copympi-module.sh
+++ yamlspecs/copympi-module.sh
@@ -4,24 +4,12 @@ OMPI_MODULE=/etc/modulefiles/mpi/openmpi-x86_64
 SKEL_MODULE=openmpi-module.yaml.in
 VER=$(rpm -qi $(rpm -qf $OMPI_MODULE) | awk -F : '/^Version/{print $NF}')
 VERSION="  version: $VER" 
-BUILD='  # Define the key components of the environment module
-  instdir: "{{root }}"
-  spec: "{{compiler}}.{{compiler_version}}"
-  module:
-    logname: "{{name}}/{{version}}/{{spec}}"
-    name: "{{spec}}"
-    path: "{{instdir}}/{{version}}"'
 SETENV='    setenv: '
 PREPEND='    prepend_path: '
-FILES='  files:
-    - "{{instdir}}"'
 
 cat $SKEL_MODULE
-echo "$VERSION"
-echo "$BUILD"
-#echo "$PREREQ"
 echo "$SETENV" 
 awk '/setenv/{print "      - " $2, $3}' $OMPI_MODULE
 echo "$PREPEND" 
 awk '/prepend-path/{print "      - " $2, $3}' $OMPI_MODULE
-echo "$FILES"
+echo "$VERSION"
